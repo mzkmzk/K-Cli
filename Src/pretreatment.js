90204c37e05a9984bf73f4bd533e49e1219a072e
maizhikun <maizhikun@xunlei.com>
2017-02-25 15:31:32 +0800

    add tips

--- Src/pretreatment.js
+++ Src/pretreatment.js
@@ -1,12 +1,14 @@
 var child_process = require('child_process'),
     Pretreatment = function(){}
 
-Pretreatment.prototype.copy = function(src_path){
+Pretreatment.prototype.copy = function(src_path, out_path){
     var child,
-        path = require('path');
+        path = require('path'),
+        child_string,
+        params = ['-r', src_path  ,out_path];
 
-    child = child_process.spawn('cp', ['-r', src_path  ,'./']);
-
+    child = child_process.spawn('cp', params);
+    console.log('cp '+ params.join(' '));
     child.stdout.on('data', (data) => {
       console.log('stdout: ',data );
     });
